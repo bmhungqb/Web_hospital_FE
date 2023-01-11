dafc405729cddd509c99d9064f7ea2a266ce8b54
bmhungqb <21522110@gm.uit.edu.vn>
2023-01-11 09:23:24 +0700

    #33

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -16,8 +16,29 @@ class Login extends Component {
         return (
             <div className='login-background'>
                 <div className="login-container">
-                    <div className='login-content'>
-
+                    <div className='login-content row'>
+                        <div className='col-12 login-text'>Login</div>
+                        <div className='col-12 form-group login-input'>
+                            <label>Username</label>
+                            <input type="text" className='form-control' placeholder='Enter your username' />
+                        </div>
+                        <div className='col-12 form-group login-input'>
+                            <label>Password</label>
+                            <input type="password" className='form-control' placeholder='Enter your password' />
+                        </div>
+                        <div className='col-12'>
+                            <button className='btn-login'>Login</button>
+                        </div>
+                        <div className='col-12'>
+                            <span className='forgot-password'>Forgot your password?</span>
+                        </div>
+                        <div className='col-12 text-center'>
+                            <span className='login-text-other'>Or Login with:</span>
+                        </div>
+                        <div className='col-12 login-social'>
+                            <i className="fab fa-google-plus-g google"></i>
+                            <i className="fab fa-facebook-f facebook"></i>
+                        </div>
                     </div>
                 </div>
             </div>
