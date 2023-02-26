f5725b939513d3454c8550b3d33dbd44ea800261
bmhungqb <21522110@gm.uit.edu.vn>
2023-02-26 13:35:16 +0700

    #93

--- src/containers/Patient/Doctor/DoctorExtraInfor.js
+++ src/containers/Patient/Doctor/DoctorExtraInfor.js
@@ -14,6 +14,14 @@ class DoctorExtraInfor extends Component {
         }
     }
     async componentDidMount() {
+        if (this.props.doctorIdFromParen) {
+            let res = await getExtraInforDoctorById(this.props.doctorIdFromParent)
+            if (res && res.errCode === 0) {
+                this.setState({
+                    extraInfor: res.data
+                })
+            }
+        }
     }
     async componentDidUpdate(prevProps, prevState, snapshot) {
         if (prevProps.language !== this.props.language) {
